4aac5457bd1488abda42aa16ff7527d2852c81e2
Hyun Ji  Kim <dear.hyunji@gmail.com>
2020-07-18 23:13:32 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -48,7 +48,7 @@ def add_characters
          {name: "Nurse", age: "44", attitude: "worried"}
          ]
       },
-      :additional_characters => [{:name=>"Prince Escalus"}, {"Apothecary"}]
+      :additional_characters => [{:name=>"Prince Escalus"}, {:name=>"Apothecary"}]
    }
 
    # Write code below that does the following programmatically:
